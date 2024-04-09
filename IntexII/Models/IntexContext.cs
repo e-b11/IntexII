5f57a327ec70c0c8da84afd5eda8ff3d01f62476
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-04-08 18:47:08 -0600

    add intexcontext

---
+++ IntexII/Models/IntexContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntexII.Models
+{
+    public class IntexContext : DbContext
+    {
+        public IntexContext(DbContextOptions<IntexContext> options) : base(options) { }
+    }
+}
--- IntexII/Program.cs
+++ IntexII/Program.cs
@@ -1,4 +1,5 @@
 using IntexII.Data;
+using IntexII.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<IntexContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
