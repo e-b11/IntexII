0ae0e53c598bac66bcba9b3a28aab1659b9536b5
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-04-09 13:40:28 -0600

    ordered model attributes to match csv

--- IntexII/Models/Order.cs
+++ IntexII/Models/Order.cs
@@ -13,13 +13,13 @@ namespace IntexII.Models
         public DateTime OrderDate {  get; set; }
         public string DayOfWeek { get; set; }
         public int Time { get; set; }
-        public string TypeOfCard { get; set; }
         public string EntryMode { get; set; }
         public double Amount { get; set; }
         public string TypeOfTransaction { get; set; }
         public string CountryOfTransaction { get; set; }
         public string ShippingAddress { get; set; }
         public string Bank { get; set; }
+        public string TypeOfCard { get; set; }
         public bool Fraud { get; set; }
     }
 }
