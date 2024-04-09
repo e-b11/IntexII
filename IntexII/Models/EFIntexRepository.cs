d3dff15df3b49f4a3d4ed61e8541bc80d1d9a23b
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-04-09 14:20:27 -0600

    started all that repository stuff. Only fully made the getAllProducts() method

--- IntexII/Models/EFIntexRepository.cs
+++ IntexII/Models/EFIntexRepository.cs
@@ -8,5 +8,14 @@
         {
             _context = temp;
         }
+
+        IQueryable<Customer> IIntexRepository.Customers => _context.Customers;
+        IQueryable<Product> IIntexRepository.Products => _context.Products;
+        public List<Product> getAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+        IQueryable<Order> IIntexRepository.Orders => _context.Orders;
+        IQueryable<LineItem> IIntexRepository.LineItems => _context.LineItems;
     }
 }
--- IntexII/Models/IntexContext.cs
+++ IntexII/Models/IntexContext.cs
@@ -5,5 +5,13 @@ namespace IntexII.Models
     public class IntexContext : DbContext
     {
         public IntexContext(DbContextOptions<IntexContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<LineItem> LineItems { get; set; }
     }
 }
