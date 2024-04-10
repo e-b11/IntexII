8a5169b00ee9ae5b7b030423a50f984a2d119137
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-04-09 19:24:50 -0600

    fixed user registration/login/logout/etc stuff partial admin controller stuff

--- IntexII/Controllers/AdminController.cs
+++ IntexII/Controllers/AdminController.cs
@@ -7,19 +7,57 @@ using Microsoft.EntityFrameworkCore;
 namespace IntexII.Controllers;
 public class AdminController : Controller
 {
+    private IIntexRepository _repo;
     private readonly UserManager<ApplicationUser> _userManager;
 
-    public AdminController(UserManager<ApplicationUser> userManager)
+    public AdminController(UserManager<ApplicationUser> userManager, IIntexRepository temp)
     {
         _userManager = userManager;
+        _repo = temp;
     }
 
-    public async Task<IActionResult> Index()
-        {
-            // Retrieve all users from the database
-            var users = await _userManager.Users.ToListAsync();
+    public IActionResult ManageUsers()
+    {
+        // Retrieve all users from the database
+        var users = _userManager.Users.ToList();
 
-            // Pass the list of users to the view
-            return View(users);
-        }
+        // Pass the list of users to the view
+        return View(users);
+    }
+    [HttpGet]
+    public IActionResult EditUser(string userId)
+    {
+      var user = _userManager.Users.Single(u => u.Id == userId);
+      return View(user);
+    }
+
+    [HttpPost]
+    public IActionResult EditUser(ApplicationUser user)
+    {
+      _userManager.UpdateAsync(user);
+      return RedirectToAction("Admin", "ManageUsers");
+
+    }
+    public IActionResult ConfirmDeleteUser(string userId)
+    {
+      var user = _userManager.Users.Single(u => u.Id == userId);
+      return View(user);
+    }
+    public IActionResult DeleteUser(ApplicationUser user)
+    {
+      // _userManager.Users.Remove(user);
+      return RedirectToAction("Admin", "ManageUsers");
+    }
+
+    public IActionResult ManageProducts()
+    {
+      var products = _repo.Products.ToList();
+      return View(products);
+    }
+    [HttpGet]
+    public IActionResult EditProduct(int productId)
+    {
+      var product = _repo.Products.Single(p => p.ProductId == productId);
+      return View(product);
+    }
 }
