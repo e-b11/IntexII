ea82405d9222052dbf568042b4ec789e689c5995
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-04-11 14:38:54 -0600

    fixed onnx directory issue and we can now manage user roles!!

--- IntexII/Controllers/AdminController.cs
+++ IntexII/Controllers/AdminController.cs
@@ -7,20 +7,28 @@ using IntexII.ViewModels;
 using IntexII.Models.ViewModels;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace IntexII.Controllers;
 public class AdminController : Controller
 {
     private IIntexRepository _repo;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
     private readonly InferenceSession _session;
+    private readonly IWebHostEnvironment _environment;
     
 
-    public AdminController(UserManager<ApplicationUser> userManager, IIntexRepository temp)
+    public AdminController(UserManager<ApplicationUser> userManager, IIntexRepository temp, IWebHostEnvironment environment, RoleManager<IdentityRole> roleManager)
     {
         _userManager = userManager;
+        _roleManager = roleManager;
         _repo = temp;
-        _session = new InferenceSession("/Users/javierdlrc/Documents/BYU/#6 Semester/IS 413/RiderProjects/IntexII/IntexII/decision_tree_model.onnx");
+        _environment = environment;
+        // Combine the base path with the relative path to your ONNX file
+        string modelPath = Path.Combine(_environment.ContentRootPath, "decision_tree_model.onnx");
+        _session = new InferenceSession(modelPath);
     }
 
     public IActionResult Home()
@@ -36,19 +44,64 @@ public class AdminController : Controller
         return View(users);
     }
     [HttpGet]
-    public IActionResult EditUser(string id)
+    public async Task<IActionResult> EditUser(string id)
     {
-      var user = _userManager.Users.Single(u => u.Id == id);
-      return View(user);
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        // Get all roles
+        var roles = _roleManager.Roles.Select(r => r.Name).ToList();
+
+        // Get the roles the user is currently in
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        // Pass user, roles, and user's current roles to the view
+        var model = new EditUserViewModel
+        {
+            User = user,
+            Roles = roles,
+            SelectedRoles = userRoles.ToList()
+        };
+
+        return View(model);
     }
+
+
 
     [HttpPost]
-    public IActionResult EditUser(ApplicationUser user)
+    public async Task<IActionResult> EditUser(EditUserViewModel model)
     {
-      _userManager.UpdateAsync(user);
-      return RedirectToAction("Admin", "ManageUsers");
+      var user = await _userManager.FindByIdAsync(model.User.Id);
+      if (user == null)
+      {
+          return NotFound();
+      }
 
+      // Update user properties
+      user.Email = model.User.Email;
+      user.CustomerId = model.User.CustomerId;
+
+      // Update user roles
+      var userRoles = await _userManager.GetRolesAsync(user);
+      var result = await _userManager.RemoveFromRolesAsync(user, userRoles);
+      if (!result.Succeeded)
+      {
+          // Handle error
+      }
+
+      result = await _userManager.AddToRolesAsync(user, model.SelectedRoles);
+      if (!result.Succeeded)
+      {
+          // Handle error
+      }
+
+      await _userManager.UpdateAsync(user);
+      return RedirectToAction("ManageUsers");
     }
+
     public IActionResult ConfirmDeleteUser(string id)
     {
       var user = _userManager.Users.Single(u => u.Id == id);
